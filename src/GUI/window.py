0ffe8a69acc3e550f137052bdce752ceda49ac77
shidqizh <shidqizh@gmail.com>
2022-11-05 18:02:07 +0700

    initiate gui

---
+++ src/GUI/window.py
@@ -0,0 +1,54 @@
+from tkinter import *
+
+
+def btn_clicked():
+    print("Button Clicked")
+
+
+window = Tk()
+
+window.geometry("1100x600")
+window.configure(bg = "#fffffa")
+canvas = Canvas(
+    window,
+    bg = "#fffffa",
+    height = 600,
+    width = 1100,
+    bd = 0,
+    highlightthickness = 0,
+    relief = "ridge")
+canvas.place(x = 0, y = 0)
+
+img0 = PhotoImage(file = f"img0.png")
+b0 = Button(
+    image = img0,
+    borderwidth = 0,
+    highlightthickness = 0,
+    command = btn_clicked,
+    relief = "flat")
+
+b0.place(
+    x = 97, y = 260,
+    width = 85,
+    height = 31)
+
+img1 = PhotoImage(file = f"img1.png")
+b1 = Button(
+    image = img1,
+    borderwidth = 0,
+    highlightthickness = 0,
+    command = btn_clicked,
+    relief = "flat")
+
+b1.place(
+    x = 97, y = 385,
+    width = 85,
+    height = 31)
+
+background_img = PhotoImage(file = f"background.png")
+background = canvas.create_image(
+    550.0, 299.5,
+    image=background_img)
+
+window.resizable(False, False)
+window.mainloop()
