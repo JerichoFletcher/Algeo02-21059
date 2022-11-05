9a0051d6037678186d6548d571b5248165c1c770
JerichoFletcher <jericho.russel17@gmail.com>
2022-11-05 18:39:30 +0700

    Main program

--- src/GUI/window.py
+++ src/GUI/window.py
@@ -12,50 +12,51 @@ def folders():
     root.withdraw()
     folder = filedialog.askdirectory()
 
-window = Tk()
+def init_window():
+    window = Tk()
 
-window.geometry("1100x600")
-window.configure(bg = "#fffffa")
-canvas = Canvas(
-    window,
-    bg = "#fffffa",
-    height = 600,
-    width = 1100,
-    bd = 0,
-    highlightthickness = 0,
-    relief = "ridge")
-canvas.place(x = 0, y = 0)
+    window.geometry("1100x600")
+    window.configure(bg = "#fffffa")
+    canvas = Canvas(
+        window,
+        bg = "#fffffa",
+        height = 600,
+        width = 1100,
+        bd = 0,
+        highlightthickness = 0,
+        relief = "ridge")
+    canvas.place(x = 0, y = 0)
 
-img0 = PhotoImage(file = f"img0.png")
-b0 = Button(
-    image = img0,
-    borderwidth = 0,
-    highlightthickness = 0,
-    command = folders,
-    relief = "flat")
+    img0 = PhotoImage(file = f"GUI/img0.png")
+    b0 = Button(
+        image = img0,
+        borderwidth = 0,
+        highlightthickness = 0,
+        command = folders,
+        relief = "flat")
 
-b0.place(
-    x = 97, y = 260,
-    width = 85,
-    height = 31)
+    b0.place(
+        x = 97, y = 260,
+        width = 85,
+        height = 31)
 
-img1 = PhotoImage(file = f"img1.png")
-b1 = Button(
-    image = img1,
-    borderwidth = 0,
-    highlightthickness = 0,
-    command = files,
-    relief = "flat")
+    img1 = PhotoImage(file = f"GUI/img1.png")
+    b1 = Button(
+        image = img1,
+        borderwidth = 0,
+        highlightthickness = 0,
+        command = files,
+        relief = "flat")
 
-b1.place(
-    x = 97, y = 385,
-    width = 85,
-    height = 31)
+    b1.place(
+        x = 97, y = 385,
+        width = 85,
+        height = 31)
 
-background_img = PhotoImage(file = f"background.png")
-background = canvas.create_image(
-    550.0, 299.5,
-    image=background_img)
+    background_img = PhotoImage(file = f"GUI/background.png")
+    background = canvas.create_image(
+        550.0, 299.5,
+        image=background_img)
 
-window.resizable(False, False)
-window.mainloop()
+    window.resizable(False, False)
+    window.mainloop()
---
+++ src/main.py
@@ -0,0 +1,8 @@
+import sys
+sys.path.append('GUI')
+
+import window
+
+## MAIN
+if __name__ == '__main__':
+    window.init_window()
