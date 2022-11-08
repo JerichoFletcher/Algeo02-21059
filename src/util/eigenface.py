f0058745cbe5557e3295a478a2d945d755350985
arleenchr <16521172@mahasiswa.itb.ac.id>
2022-11-08 20:34:17 +0700

    Update eigenface.py

--- src/util/eigenface.py
+++ src/util/eigenface.py
@@ -1,33 +1,62 @@
 import numpy as np
 from processimage import *
 
+N = 256
+
 def eigenface(dataset):
+    print(dataset)
     # dataset adalah array of matriks gambar
     # mencari mean dari semua matriks dataset
-    sumMatrix = []
-    for i in range (0,len(dataset)-1):
-        sumMatrix += dataset[i]
+    sumMatrix = [[0 for j in range(N)] for i in range(N)]
+    for i in range (0,len(dataset)):
+        sumMatrix = sumMatrix + dataset[i]
+        #print("sum matrix")
+        #print(sumMatrix)
     meanDataset = sumMatrix / len(dataset)
+    #print("mean data set")
+    #print(meanDataset)
     
     # cari selisih dari tiap matriks dataset dengan meanDataset
     selisih = []
-    for i in range (0,len(dataset)-1):
+    for i in range (0,len(dataset)):
         selisih.append(dataset[i] - meanDataset)
+    #print("selisih")
+    #print(selisih)
     
     # hitung nilai matriks kovarian: rata rata dari perkalian antara matriks selisih dengan transposnya
     arrCovarian = []
-    for i in range (0,len(selisih)-1):
-        arrCovarian.append(np.matmul(selisih[i],np.selisih[i].transpose()))
-    sumCovarian = []
-    for i in range (0,len(arrCovarian)-1):
-        sumCovarian += arrCovarian[i]
-    covarian = sumCovarian / len(arrCovarian)
+    for i in range (0,len(selisih)):
+        covarianMult = [[0 for j in range(N)] for i in range(N)]
+        covarianMult = np.matmul(selisih[i],selisih[i].transpose()) # salah di sini
+        arrCovarian.append(covarianMult)
+    #print("arr covarian selisih dikali transposnya")
+    #print(arrCovarian)
+    sumCovarian = [[0 for j in range(N)] for i in range(N)]
+    for i in range (0,len(arrCovarian)):
+        sumCovarian = sumCovarian + arrCovarian[i]
+    #print("sum covarian")
+    #print(sumCovarian)
+    
+    covarian = [[0 for j in range(N)] for i in range(N)]
+    covarian = sumCovarian * (1 / len(arrCovarian)) # masih error di sini
+    #print("covarian")
+    #print(covarian)
     
     # cari eigen value dan eigen vector dari covarian
     eigenValue,eigenVector=np.linalg.eig(covarian)
-    
+    #print("eigen vector")
+    #print(eigenVector)
     # cari eigen face dengan mengalikan eigen vector dengan selisih masing masing foto, lalu jumlahkan
-    EigenFace = []
-    for i in range (0,len(dataset)-1):
-        EigenFace += (np.matmul(eigenVector,selisih[i]))
+    EigenFace = [[0 for j in range(N)] for i in range(N)]
+    for i in range (0,len(selisih)):
+        #print("selisih")
+        #print(selisih[i])
+        eigFaceMult = np.matmul(eigenVector,selisih[i])
+        #print("eigFaceMult")
+        #print(eigFaceMult)
+        EigenFace += (eigFaceMult)
+        #print("eigen faceeee")
+        #print(EigenFace)
+    #print("eigen face hasil")
+    #print(EigenFace)
         
