499438359b45709a3562dc427d43e6f8b53b914b
arleenchr <16521172@mahasiswa.itb.ac.id>
2022-11-18 00:03:08 +0700

    update eigenface
    unfinished

--- src/util/eigenface.py
+++ src/util/eigenface.py
@@ -1,24 +1,31 @@
 import numpy as np
+from eigenpair import *
 
 #N = 3
+#dataset
+datasetVector = []
+meanDataset = []
+eigenFaceVector = []
 
 def matrixToVector(m):
     # Mengubah matriks m (N x N) menjadi vektor (N^2 x 1)
     #v = np.array([0] for i in range (len(m)**2))
     v = np.zeros(((len(m))**2, 1))
     #v = np.array([[0] for i in range (0,len(m)**2)])
-    count = 0
-    for i in range (0,len(m)):
-        for j in range (0,len(m)):
-            v[(i+j+2*count)][0] = m[i][j]
-        count += 1
+    for j in range (len(m)):
+        for i in range (len(m)):
+            v[i+j*(len(m))][0] = m[i][j]
     return v
 
 def vectorToMatrix(v):
     # vector (N^2 x 1) to matrix (N x N)
     m = np.array([[0. for j in range (int(len(v) ** 0.5))] for i in range (int(len(v) ** 0.5))])
+    for i in range (len(v)):
+        m[i%(int(len(v)**0.5))][i//(int(len(v)**0.5))] = v[i][0]
+    '''
     for i in range (0,len(v)):
         m[i//(int(len(v)**0.5))][i%(int(len(v)**0.5))] = v[i]
+    '''
     return m
 
 def packToMatrix(arrayM):
@@ -66,27 +73,50 @@ def eigenface(dataset):
     #print("A transpose")
     #print(AT)
     covarian = np.matmul(AT,A)
-    #print("covarian")
-    #print(covarian)
+    #covarian = np.matmul(A,AT)
+    print("covarian")
+    print(covarian)
     
     # cari eigen value dan eigen vector dari covarian
     #eigenVector = np.array([0.0,0.0] for x in len(dataset))
-    eigenValue,eigenVector=np.linalg.eig(covarian)
-    #print("eigen vector")
-    #print(eigenVector)
+    #eigenValue,eigenVector=np.linalg.eig(covarian)
+    eigenValue,eigenVector=extractEigenpairsQR(covarian)
+    #print("eigenValue")
+    #print(eigenValue)
+    print("eigen vector")
+    print(eigenVector)
+    
+    # cari magnitude eigen vector
+    magnitudeEigenVector = np.array([0. for i in range (len(eigenVector))])
+    for i in range (len(magnitudeEigenVector)):
+        sumMagnitude = 0
+        for j in range (len(eigenVector[0])):
+            sumMagnitude += (eigenVector[i][j] ** 2)
+        magnitudeEigenVector[i] = (sumMagnitude ** 0.5)
+    print("magnitudeEigenVector")
+    print(magnitudeEigenVector)
     
     # cari eigenface dengan mengalikan A dengan setiap eigen vector
-    arrEigenface = np.array([[0. for i in range(len(dataset[0])**2)] for x in range (len(dataset))])
+    eigenFaceVector = np.array([[0. for i in range(len(dataset[0])**2)] for x in range (len(dataset))])
     for i in range (len(dataset)):
-        arrEigenface[i] = np.matmul(A,eigenVector[i])
-    #print("eigenface")
-    #print(arrEigenface)
+        eigenFaceVector[i] = np.matmul(A,eigenVector[i])
+    print("eigenfaceVector")
+    print(eigenFaceVector)
+    '''return eigenFaceVector'''
     
     # ubdah bentuk eigenface yang dalam bentuk vector (N^2 x 1) menjadi (N x N)
-    retEigenface = np.array([[[0. for j in range (len(dataset[0]))] for i in range(len(dataset[0]))] for x in range (len(dataset))])
-    for i in range (len(dataset)):
-        retEigenface[i] = (vectorToMatrix(arrEigenface[i]))
+    #retEigenface = np.array([[[0. for j in range (len(dataset[0]))] for i in range(len(dataset[0]))] for x in range (len(dataset))])
+    #for i in range (len(dataset)):
+    #    retEigenface[i] = (vectorToMatrix(arrEigenface[i]))
     #print("retEigenface")
     #print(retEigenface)
-    return retEigenface
+    #return retEigenface
+
+def ruangWajah():
+    # menghitung ruang wajah
+    arrRuangWajah = np.array([[0] for i in range (len(datasetVector))])
+    for i in range (len(arrRuangWajah)):
+        arrRuangWajah[i] = np.matmul(np.transpose(eigenFaceVector[i]), np.subtract(datasetVector[i], meanDataset))
+    return arrRuangWajah
+
         
